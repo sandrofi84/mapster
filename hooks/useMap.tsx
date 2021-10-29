34a8b01494f3a124407a5a0fadd98d428e917baa
Sandro <sandrofillinich@gmail.com>
2021-10-29 16:53:07 +0100

    feat: create useMap hook

--- hooks/useMap.tsx
+++ hooks/useMap.tsx
@@ -1,14 +1,15 @@
-import React, { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 mapboxgl.accessToken = process.env.MAPBOX_ACCESS_TOKEN as string;
 
-const Map = () => {
+const useMap = () => {
   const mapContainer = useRef<string | HTMLElement>('');
   const map = useRef<null | mapboxgl.Map>(null);
   const [lng, setLng] = useState(-70.9);
   const [lat, setLat] = useState(42.35);
   const [zoom, setZoom] = useState(9);
+  const [isMapInitialized, setIsMapInitialized] = useState(false);
 
   useEffect(() => {
     if (map.current) return; // initialize map only once
@@ -18,11 +19,11 @@ const Map = () => {
       center: [lng, lat],
       zoom: zoom,
     });
+
+    setIsMapInitialized(true);
   }, []);
 
-  return (
-    <div ref={mapContainer} style={{ width: '100vw', height: '500px' }}></div>
-  );
+  return { map: map.current, mapContainer, isMapInitialized };
 };
 
-export default Map;
+export default useMap;
